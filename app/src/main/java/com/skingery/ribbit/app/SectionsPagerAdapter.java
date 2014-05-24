97e7bc7da07c3f2a3ac28559d20b2d2c444d6cb3
sKingery <seankingery@gmail.com>
2014-05-24 17:09:16 -0400

    Added Inbox and friends tabs using fragments. features now work. Oops forgot to add some files.

---
+++ app/src/main/java/com/skingery/ribbit/app/SectionsPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.skingery.ribbit.app;
+
+
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.Locale;
+
+/**
+ * A {@link android.support.v13.app.FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    protected Context mContext; // create a member variable for the context
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+
+        mContext = context; // mContext to the context variable
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+
+        // add switch statement to switch between fragments depending on the postion
+        switch (position){
+            case 0: return new InboxFragment();
+            case 1: return new FriendsFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.title_section1).toUpperCase(l);
+            case 1:
+                return mContext.getString(R.string.title_section2).toUpperCase(l);
+        }
+        return null;
+    }
+}
+
